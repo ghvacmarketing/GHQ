1ac553f9b70b3b29ffadce9a9d73a08c060fb84c
shelbgies <45658379-shelbgies@users.noreply.replit.com>
2025-10-05 20:37:13 +0000

    Restored to '0aaa9cd505bdd5bc6e250b1757640e7cc60c250f'
    Replit-Restored-To: 0aaa9cd505bdd5bc6e250b1757640e7cc60c250f

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,3 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-// Unregister any existing service workers to clear cache
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
